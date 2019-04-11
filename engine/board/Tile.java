dce18b9492f632bbad4ee23ad985f91706db650c
tonyDevy <46561396+tonyDevy@users.noreply.github.com>
2019-04-11 05:04:23 -0400

    allClasses

---
+++ engine/board/Tile.java
@@ -0,0 +1,87 @@
+
+package com.chess.engine.board;
+import com.chess.engine.pieces.Piece;
+import com.google.common.collect.ImmutableMap;
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+This is the Tile class of Chess Game
+Inside Tile class, you have 2 Subclasses
+EmptyTile - Returns empty tile
+OccupiedTile - Returns tile coordinate, and piece
+*/
+
+public abstract class Tile {
+    
+    protected final int tileCoordinate;
+    
+    private static final Map<Integer, EmptyTile> EMPTY_TILES_CACHE = creatAllPossibleEmptyTiles();
+    
+    private static Map<Integer, EmptyTile> creatAllPossibleEmptyTiles() {
+        final Map<Integer, EmptyTile> emptyTileMap = new HashMap();
+        for(int i = 0; i < BoardUtils.NUM_TILES; i++){   
+            emptyTileMap.put(i, new EmptyTile(i));   
+        }
+        return ImmutableMap.copyOf(emptyTileMap);
+    }
+    public static Tile createTile(final int tileCoordinate, final Piece piece){
+        return piece != null ? new OccupiedTile(tileCoordinate, piece) : EMPTY_TILES_CACHE.get(tileCoordinate);
+    }
+    private Tile(final int tileCoordinate) {
+        this.tileCoordinate = tileCoordinate;
+    }
+    public abstract boolean isTileOccupied();
+    
+    public abstract Piece getPiece(); 
+    
+    public int getTileCoordinate(){
+        return this.tileCoordinate;
+    }
+        
+public static final class EmptyTile extends Tile{
+    
+    private EmptyTile(final int coordinate){
+        super(coordinate);  
+    }
+    
+    @Override
+    public String toString(){
+        return "-";
+    }
+    @Override
+    public boolean isTileOccupied(){
+        return false;
+    }
+    @Override
+    public Piece getPiece(){
+        return null;
+    }
+}  
+public static final class OccupiedTile extends Tile{
+    private final Piece pieceOnTile;
+    
+    private OccupiedTile(int tileCoordinate, final Piece pieceOnTile){
+        super(tileCoordinate);
+        this.pieceOnTile = pieceOnTile;
+    }
+    
+    @Override
+    public String toString(){
+        return getPiece().getPieceAlliance().isBlack() ? 
+               getPiece().toString().toLowerCase() : 
+               getPiece().toString();
+    }
+    @Override
+    public boolean isTileOccupied(){
+        return true;
+    }
+    @Override
+    public Piece getPiece(){
+        return pieceOnTile;
+    }
+  }
+}
+
+
+    
